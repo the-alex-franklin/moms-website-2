f89e5a1e718f79786a7c0c12662879ddc28f858a
Alex <the.alex.franklin@gmail.com>
2025-08-11 15:31:19 -0700

    chatGPT-5 first suggestion

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import '@unocss/reset/tailwind.css'
-import 'virtual:uno.css'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
+import './styles.css'
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    element: <App />, children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/services', element: <Services /> },
+      { path: '/gallery', element: <Gallery /> },
+      { path: '/contact', element: <Contact /> },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
