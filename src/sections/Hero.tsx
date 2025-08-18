e79a6faa8a7c1641f1cac5e6190bf08598a7eeae
Alex <the.alex.franklin@gmail.com>
2025-08-18 13:43:23 -0700

    little touch ups

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,7 +3,7 @@ export function Hero() {
     <section className="relative flex items-end sm:items-center justify-center text-center h-[80vw] max-h-[92vh] bg-[url('/hero.jpg')] bg-cover bg-[position:0_35%] overflow-hidden">
       <div className="absolute inset-0 bg-black/40"></div>
 
-      <div className="flex flex-col relative px-4 z-10 max-w-[700px] text-white translate-y-36">
+      <div className="flex flex-col relative px-4 z-10 max-w-[700px] text-white sm:translate-y-36">
         <h1 className="text-white text-5xl font-bold font-calli mb-4">Whim Therapy</h1>
         <p className="text-2xl mb-8 font-calli">In-home physical therapy</p>
       </div>
